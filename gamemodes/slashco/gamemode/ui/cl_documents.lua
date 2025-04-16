637fed86b27df27a4fa3622f3d993d3ea58079dd
FirstLemon <obivannix@gmail.com>
2025-04-16 14:51:42 +0200

    documents: fixed helper ui text going into screen when different anything

--- gamemodes/slashco/gamemode/ui/cl_documents.lua
+++ gamemodes/slashco/gamemode/ui/cl_documents.lua
@@ -356,8 +356,8 @@ hook.Add("PostDrawOpaqueRenderables", "LobbyDocumentScreen", function(bDrawingDe
 		surface.DrawRect(0, 0, w, h)
 
 		-- ToDo: Make The text align to the screen properly or else this happens: https://i.imgur.com/TlvTEnX.png
-		draw.SimpleText(SlashCo.Language("documents_ui_left_click"), "TVCD", w * 1.55, (h / 2), color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
-		draw.SimpleText(SlashCo.Language("documents_ui_right_click"), "TVCD", w * 1.55, (h / 2) + (h / 20), color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
+		draw.SimpleText(SlashCo.Language("documents_ui_left_click"), "TVCD", w * 1, (h / 2), color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
+		draw.SimpleText(SlashCo.Language("documents_ui_right_click"), "TVCD", w * 1, (h / 2) + (h / 20), color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT)
 
 		-- Debug to check screen Mins/Maxs values
 		-- debugoverlay.BoxAngles( screenPos, screenMins, screenMaxs, screenAngle, 0.02, hitPos != nil and Color(0,255,0) or Color( 255,0, 0, 10) )
